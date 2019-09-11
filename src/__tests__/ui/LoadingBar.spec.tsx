22e805f551964d0d4c9c5ac0da5c449d6105539c
Travis Valenti <travis@travisvalenti.com>
2019-09-11 21:19:44 +1000

    Switch to react-testing-library

---
+++ src/__tests__/ui/LoadingBar.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import LoadingBar from '../../components/ui/LoadingBar'
+
+describe('LoadingBar component', () => {
+  test('Matches the snapshot', () => {
+    const loadingBar = render(<LoadingBar />)
+    expect(loadingBar.container).toMatchSnapshot()
+  })
+})
