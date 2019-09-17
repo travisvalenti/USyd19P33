af646281469158342310cc5c48f588740920c05c
Yuan <dreamy0409@outlook.com>
2019-09-17 17:00:00 +1000

    download attchment ver 0.3 adding some properties to MessageType

--- src/components/Mail/Message/index.tsx
+++ src/components/Mail/Message/index.tsx
@@ -13,6 +13,9 @@ const Message: React.FC<Props> = (props: Props) => {
 
   // const content = part && atob(part.body.data) // atob decodes a Base64 string
   let content: string | undefined = ''
+
+  let attachment_data : string[] = [];
+
   if (props.isExpanded) {
     const part = props.message.payload.parts
       .find(part => part.mimeType === 'text/html')
@@ -20,20 +23,22 @@ const Message: React.FC<Props> = (props: Props) => {
       .split('-').join('+')
       .split('_').join('/')
     content = blob && atob(blob)
-  }
 
-  const downloadAttachments = (message : any) => {
-    const parts = message.payload.parts;
+    // download attchments
+    const parts = props.message.payload.parts;
+
     for (let i = 0; i < parts.length; i++) {
-      const part = parts[i];
-      if (part.filename && part.filename.length > 0) {
-        const attachId = part.body.attachmentId;
-        const request = (gapi.client as any).gmail.users.messages.attachments.get({
+      const part_ = parts[i];
+      if (part_.filename && part_.filename.length > 0) {
+        const attachId = part_.body.attachmentId;
+        return (gapi.client as any).gmail.users.messages.attachments.get({
           'id': attachId,
-          'messageId': message.id,
+          'messageId': props.message.id,
           'userId': 'me'
+        }).then(function(response : any) {
+          attachment_data.push(response.result.data);
+          console.log(response);
         });
-        request.execute(function(attachment : any){});
       }
     }
   }
@@ -47,7 +52,7 @@ const Message: React.FC<Props> = (props: Props) => {
         {props.message.labelIds.map(label => (<span key={label} className="chip">{label}</span>))}
       </div>
       {props.isExpanded && (<div className="mailItemContent">
-        <Button onClick={downloadAttachments(props.message)} className="download" disabled={false} children="Download Attachments"/>
+        <a href={"data:" + attachment_data[0]}>Download Attachment</a>
         <iframe title={props.message.id} srcDoc={content} frameBorder="0" seamless></iframe>
       </div>)}
     </div>
