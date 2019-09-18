df4098743b4482ba67abb34ff1d7d49ae1f3c454
Travis Valenti <travis@travisvalenti.com>
2019-09-18 23:34:33 +1000

    Add basic email editing page

---
+++ src/components/Write/index.tsx
@@ -0,0 +1,190 @@
+import React, { Component, ChangeEvent } from 'react'
+import {
+  Editor,
+  EditorState,
+  RichUtils,
+  getDefaultKeyBinding,
+  ContentState,
+  convertToRaw
+} from 'draft-js'
+import { convertToHTML } from 'draft-convert'
+
+import './styles.css'
+import Button from '../ui/Button'
+import LoadingBar from '../ui/LoadingBar'
+
+type State = {
+  content: string
+  markdownActive: boolean
+  editorState: EditorState
+  from: string
+  to: string
+  subject: string
+  loading: boolean
+}
+
+export default class Write extends Component<{}, State> {
+  state = {
+    content: '',
+    markdownActive: true,
+    editorState: EditorState.createEmpty(),
+    from: '',
+    to: '',
+    subject: '',
+    loading: false
+  }
+
+  editorReference: any
+
+  componentDidMount () {
+    const from = this.getFromString()
+    this.setState({ from })
+  }
+
+  getFromString = () => {
+    const basicProfile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile()
+    return `${basicProfile.getGivenName()} ${basicProfile.getFamilyName()} <${basicProfile.getEmail()}>`
+  }
+
+  onEditorStateChange = (editorState: EditorState) => {
+    this.setState({ editorState })
+  }
+
+  handleKeyCommand = (command: string, editorState: EditorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onEditorStateChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    switch (event.target.name) {
+      case 'to':
+        this.setState({ to: event.target.value })
+        break
+      case 'subject':
+        this.setState({ subject: event.target.value })
+        break
+    }
+  }
+
+  saveDraft = () => {
+    this.setState({
+      loading: true
+    })
+    const currentContent = this.state.editorState.getCurrentContent()
+
+    const blocks = convertToRaw(
+        this.state.editorState.getCurrentContent()
+      )
+      .blocks
+      .map(block => block.text)
+
+    const content = blocks.join('\n')
+
+    const html = convertToHTML({})(currentContent)
+
+    let mime = `From: ${this.getFromString()}${this.state.to && '\nTo:'} ${this.state.to}${this.state.subject && '\nSubject:'} ${this.state.subject}
+MIME-Version: 1.0
+Content-Type: multipart/mixed; 
+    boundary=gc0p4Jq0M2Yt08jU534c0p
+
+${content}
+
+--gc0p4Jq0M2Yt08jU534c0p
+Content-Type: text/plain;
+
+${content}
+
+--gc0p4Jq0M2Yt08jU534c0p
+Content-Type: text/html;
+
+${html}
+
+--gc0p4Jq0M2Yt08jU534c0p--`
+
+    console.log(mime)
+
+    const response = (gapi.client as any)
+      .gmail
+      .users
+      .drafts
+      .create({
+        userId: 'me',
+        resource: {
+          message: {
+            raw: btoa(mime)
+              .split('+').join('-')
+              .split('/').join('_')
+          }
+        }
+      })
+    response.then(() => {
+        this.setState({
+          loading: false
+        })
+    })
+    .catch((error: Error) => console.error(error))
+  }
+
+  render() {
+    return (
+      <div className="Write">
+        <div>
+          <div className="editor">
+            <strong>From</strong>
+            <div className="from">
+              { this.state.from }
+            </div>
+            <strong>To</strong>
+            <div className="to">
+              <input type="text" name="to" value={this.state.to} onChange={this.onInputChange}/>
+            </div>
+            <strong>Subject</strong>
+            <div className="subject">
+              <input type="text" name="subject" value={this.state.subject} onChange={this.onInputChange}/>
+            </div>
+            <strong>Message</strong>
+            <div className="editorToolbar">
+              <i className={ 'material-icons' } onClick={() =>
+                this.onEditorStateChange(EditorState.push(this.state.editorState, ContentState.createFromText(''), 'remove-range'))
+              }>delete</i>
+              <i className={ 'material-icons' + (this.state.editorState.getCurrentInlineStyle().contains('ITALIC') ? ' active' : '') } onClick={() =>
+                this.onEditorStateChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+              }>format_italic</i>
+              <i className={ 'material-icons' + (this.state.editorState.getCurrentInlineStyle().contains('BOLD') ? ' active' : '') } onClick={() =>
+                this.onEditorStateChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+              }>format_bold</i>
+              <i className={ 'material-icons' + (this.state.editorState.getCurrentInlineStyle().contains('STRIKETHROUGH') ? ' active' : '') } onClick={() =>
+                this.onEditorStateChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'))
+              }>format_strikethrough</i>
+              <i className={ 'material-icons' + (this.state.editorState.getCurrentInlineStyle().contains('UNDERLINE') ? ' active' : '') } onClick={() =>
+                this.onEditorStateChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
+              }>format_underline</i>
+            </div>
+            <div className="textArea" onClick={() =>
+              this.editorReference.focus()
+            }>
+              <Editor
+                ref={ref => this.editorReference = ref}
+                editorState={this.state.editorState}
+                onChange={this.onEditorStateChange}
+                handleKeyCommand={this.handleKeyCommand}
+                keyBindingFn={getDefaultKeyBinding}
+              />
+            </div>
+            <div className="actionButtons">
+              <Button onClick={this.saveDraft}>Save Draft</Button>
+              <Button>Send</Button>
+            </div>
+            <div style={{ height: '1rem' }}>
+              { this.state.loading &&  <LoadingBar /> }
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
