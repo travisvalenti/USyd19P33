75a6cca43335a602ed0cfea497d549abffc83d4f
Travis Valenti <travis@travisvalenti.com>
2019-09-18 17:06:02 +1000

    Update config with write permissions for gapi

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export default {
       clientId: '89646939632-a5i06tvc4np3vj82m0h1nlept16prom8.apps.googleusercontent.com',
       apiKey: 'AIzaSyBpdgjg3vJYGKcFn4DcxVbi2AEBMeE_KX4',
       discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest'],
-      scope: 'https://www.googleapis.com/auth/gmail.readonly',
+      scope: 'https://mail.google.com/',
     }
   }
 }
